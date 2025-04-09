9b1c8282061e5b211703dfcb6094e30a3c74bc12
Andreea-Daniela Ghenu <andreea.ghenu@welink.ro>
2025-04-09 16:34:21 +0300

    refactor: implement useEffect for App and Modal components

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // Functions as useEffect dependencies can trigger reruns on every render
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      onConfirm();
+    }, 3000)
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
