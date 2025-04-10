a5cd876bfb71dcfc2135c749526a64f9ef277885
Andreea-Daniela Ghenu <andreea.ghenu@welink.ro>
2025-04-10 10:30:19 +0300

    feat: add progress bar for delete confirmation modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,11 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  // Update the remaining time every 10ms and clear the interval when the modal disappears
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 10);
+    }, 10);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   // Functions as useEffect dependencies can trigger reruns on every render
   useEffect(() => {
     const timerId = setTimeout(() => {
       onConfirm();
-    }, 3000)
+    }, TIMER);
 
     return () => {
       clearTimeout(timerId);
@@ -24,6 +37,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER}/>
     </div>
   );
 }
